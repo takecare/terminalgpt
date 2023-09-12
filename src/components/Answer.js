dcbda2053e505991ddbef3e0c7ea42986d675175
Rui Teixeira <rui@vazteixeira.org>
2023-09-12 09:20:00 +0100

    add clipboardy package to copy answers

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,15 +1,16 @@
 import { Text, useApp } from "ink";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
+import { request, fakeRequest } from "../gpt.js";
 import { Message } from "../gptcontext.js";
 import { Loading } from "./Loading.js";
-import { fakeRequest } from "../gpt.js";
+// import clipboard from "clipboardy";
 
 const Answer = ({ model, messages }) => {
   const { exit } = useApp(); // https://github.com/vadimdemedes/ink#useapp
   const [response, setResponse] = useState();
 
-  // TODO offer to copy answer?
+  // TODO offer to copy answer? what's the best ux for this?
   // TODO measure response time (debug mode)
 
   useEffect(() => {
@@ -18,6 +19,9 @@ const Answer = ({ model, messages }) => {
       // const apiResponse = await request(model, messages);
       const response = apiResponse.data.choices[0];
       setResponse(response.message.content);
+
+      // clipboard.writeSync(response.message.content);
+
       exit(); // FIXME this needs to be removed for interactive mode
     };
     get();
